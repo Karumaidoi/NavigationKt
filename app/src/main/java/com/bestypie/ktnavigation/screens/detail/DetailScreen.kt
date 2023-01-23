24f2db97a2daa5fd707c99e084b7cb1f84d72f71
Karumaidoi <mainahmwangi12@gmail.com>
2023-01-23 05:56:22 +0300

    added FAB

--- app/src/main/java/com/bestypie/ktnavigation/screens/detail/DetailScreen.kt
+++ app/src/main/java/com/bestypie/ktnavigation/screens/detail/DetailScreen.kt
@@ -1,31 +1,39 @@
 package com.bestypie.ktnavigation.screens.detail
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Phone
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
 fun DetailScreen(navController: NavController) {
     Scaffold(topBar = {
-        Row(modifier = Modifier.clickable {
-            navController.popBackStack();
-        }) {
-          Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Arrow Back")
-            Spacer(modifier = Modifier.width(20.dp))
-            Text(text = "Back", style = MaterialTheme.typography.h5)
+        TopAppBar(modifier = Modifier, backgroundColor = Color.Magenta) {
+            Row(modifier = Modifier.clickable {
+                navController.popBackStack();
+            }, horizontalArrangement = Arrangement.Start) {
+                Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Arrow Back")
+                Spacer(modifier = Modifier.width(20.dp))
+                Text(text = "Back", style = MaterialTheme.typography.h5)
+            }
+        }
+    }, floatingActionButton = {
+        FloatingActionButton(onClick = {}) {
+            Icon(imageVector = Icons.Default.Phone, contentDescription = "Phone Call", modifier = Modifier, tint = Color.White)
         }
     }) {
+    // Add Scaffold Content Here
+
 
     }
 }
